f015b86037930d11e509377e8495dd06b1c704cd
Sam Horovatin <shorovatin@live.ca>
2017-01-16 03:19:22 +0000

    Added routes for user profiles

--- saasapp/config/routes.rb
+++ saasapp/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users do
+    resource :profile
+  end
   get 'about', to: 'pages#about'
-  resources:contacts, only: :create
+  resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
